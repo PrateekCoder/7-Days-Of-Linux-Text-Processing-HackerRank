4aefa5d055a182e249a367d477db539c153176e5
Prateek_Coder <prateekgaurav23@gmail.com>
2016-02-22 19:19:38 +0530

    Add Cut #1

---
+++ 01_Cut_#1.sh
@@ -0,0 +1,31 @@
+#Objective
+#In this challenge, we practice using the cut command to get and print characters from a file.
+
+#Task
+#Given NN lines of input, print the 3rd3rd character from each line as a new line of output. It is guaranteed that each of the nn lines of input will have a 3rd3rd character.
+
+#Input Format
+
+#A text file containing NN lines of ASCII characters.
+
+#Constraints
+
+#1≤N≤1001≤N≤100
+#Output Format
+
+#For each line of input, print its 3rd3rd character on a new line for a total of NN lines of output.
+
+#Sample Input
+
+#Hello
+#World
+#how are you
+#Sample Output
+
+#l
+#r
+#w
+
+Solution:
+
+cut -c3 
